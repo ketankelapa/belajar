7e6fb51b45740c4fb957a12b38f79c3aec2001b7
Mochamad Naufal Kurnia <nkurnia1031@gmail.com>
2022-06-09 13:43:57 +0700

    update

--- ujicoba/functions.php
+++ ujicoba/functions.php
@@ -1,186 +1,186 @@
-<?php 	
+<?php
+
 //koneksi ke database
-$koneksi = mysqli_connect("localhost", "root",	"",	"data");
+$koneksi = mysqli_connect("localhost", "root", "", "data");
 
-function query($query) {
-	global $koneksi;
-	$result = mysqli_query($koneksi, $query);
-	$rows= [];
-	while( $row = mysqli_fetch_assoc($result) ) {
-		$rows [] = $row;
-	}
-	return $rows;
+function query($query)
+{
+    global $koneksi;
+    $result = mysqli_query($koneksi, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
 }
 
+function tambah($data)
+{
+    global $koneksi;
 
-function tambah ($data){
-	global $koneksi;
+    //htmlspecialchars supaya yg di masukin bukan scrip html
+    $merek = htmlspecialchars($data["merek"]);
+    $tipe = htmlspecialchars($data["tipe"]);
+    $tahun = htmlspecialchars($data["tahun"]);
+    $kondisi = htmlspecialchars($data["kondisi"]);
+    $stok = htmlspecialchars($data["stok"]);
+    $warna = htmlspecialchars($data["warna"]);
 
-	//htmlspecialchars supaya yg di masukin bukan scrip html
-	$merek =htmlspecialchars( $data["merek"]);
-	$tipe =htmlspecialchars( $data["tipe"]);
-	$tahun =htmlspecialchars( $data["tahun"]);
-	$kondisi =htmlspecialchars( $data["kondisi"]);
-	$stok = htmlspecialchars($data["stok"]);
-	$warna = htmlspecialchars($data["warna"]);
+    //upload gambar
+    $gambar = upload();
+    if (!$gambar) {
+        return false;
+    }
 
-	//upload gambar
-	$gambar = upload();
-	if (!$gambar){
-		return false;
-	}
-
-	//query insert data
-	$query = "INSERT INTO hp
+    //query insert data
+    $query = "INSERT INTO hp
 			VALUES
-			(0, '$merek', '$tipe', '$tahun', '$kondisi', '$stok', '$warna', '$gambar') 
+			(0, '$merek', '$tipe', '$tahun', '$kondisi', '$stok', '$warna', '$gambar')
 			";
-	mysqli_query($koneksi, $query);
-	return mysqli_affected_rows ($koneksi);
-
+    mysqli_query($koneksi, $query);
+    return mysqli_affected_rows($koneksi);
 
 }
 
-function upload(){
+function upload()
+{
 
-	$namaFile = $_FILES['gambar']['name'];
-	$ukuranFile = $_FILES['gambar']['size'];
-	$error = $_FILES['gambar']['error'];
-	$tmpName = $_FILES['gambar']['tmp_name'];
+    $namaFile = $_FILES['gambar']['name'];
+    $ukuranFile = $_FILES['gambar']['size'];
+    $error = $_FILES['gambar']['error'];
+    $tmpName = $_FILES['gambar']['tmp_name'];
 
-	// cek apakah tidak ada gambar yang di upload
-	if ( $error == 4 ){
-		echo "<script>
+    // cek apakah tidak ada gambar yang di upload
+    if ($error == 4) {
+        echo "<script>
 				alert('pilih gambar terlebih dahulu!');
 			</script>";
-			return false;
-	}
+        return false;
+    }
 
-	//cek apakah yang diupload adalah gambar
-	$ektensiGambarValid = ['jpg', 'jpeg', 'png'];
-	$ektensiGambar = explode('.', $namaFile);
-	$ektensiGambar = strtolower (end($ektensiGambar));
-	if( !in_array($ektensiGambar, $ektensiGambarValid)){
-		echo "<script>
+    //cek apakah yang diupload adalah gambar
+    $ektensiGambarValid = ['jpg', 'jpeg', 'png'];
+    $ektensiGambar = explode('.', $namaFile);
+    $ektensiGambar = strtolower(end($ektensiGambar));
+    if (!in_array($ektensiGambar, $ektensiGambarValid)) {
+        echo "<script>
 				alert('yang anda upload bukan gambar');
 			</script>";
-			return false;
-		}
-	//cek jika ukurannya terlalu besar
-	if ($ukuranFile > 1000000) {
-		echo "<script>
+        return false;
+    }
+    //cek jika ukurannya terlalu besar
+    if ($ukuranFile > 1000000) {
+        echo "<script>
 				alert('ukuran gambar terlalu besar');
 			</script>";
-			return false;
-	}
+        return false;
+    }
 
-	//lolos pengecekan, gambar siap di upload
-	//generate nama gambar baru
-	$namaFileBaru = uniqid();
-	$namaFileBaru .= '.';
-	$namaFileBaru .= $ektensiGambar;
+    //lolos pengecekan, gambar siap di upload
+    //generate nama gambar baru
+    $namaFileBaru = uniqid();
+    $namaFileBaru .= '.';
+    $namaFileBaru .= $ektensiGambar;
 
-	move_uploaded_file($tmpName, 'img/' . $namaFileBaru);
-	return $namaFileBaru;
+    move_uploaded_file($tmpName, 'img/' . $namaFileBaru);
+    return $namaFileBaru;
 
 }
 
-
-function hapus ($id) {
-	global $koneksi;
-	mysqli_query($koneksi, "DELETE FROM hp WHERE id = $id");
-	return mysqli_affected_rows ($koneksi);
+function hapus($id)
+{
+    global $koneksi;
+    mysqli_query($koneksi, "DELETE FROM hp WHERE id = $id");
+    return mysqli_affected_rows($koneksi);
 }
 
-function ubah ($data) {
-	global $koneksi;
+function ubah($data)
+{
+    global $koneksi;
 
-	$id = $data["id"];
-	$merek =htmlspecialchars( $data["merek"]);
-	$tipe =htmlspecialchars( $data["tipe"]);
-	$tahun =htmlspecialchars( $data["tahun"]);
-	$kondisi =htmlspecialchars( $data["kondisi"]);
-	$stok = htmlspecialchars($data["stok"]);
-	$warna = htmlspecialchars($data["warna"]);
-	$gambarlama = htmlspecialchars($data["gambarlama"]);
+    $id = $data["id"];
+    $merek = htmlspecialchars($data["merek"]);
+    $tipe = htmlspecialchars($data["tipe"]);
+    $tahun = htmlspecialchars($data["tahun"]);
+    $kondisi = htmlspecialchars($data["kondisi"]);
+    $stok = htmlspecialchars($data["stok"]);
+    $warna = htmlspecialchars($data["warna"]);
+    $gambarlama = htmlspecialchars($data["gambarlama"]);
 
-	//cek apakah use pilih gambar baru atau tidak
-	if ($_FILES['gambar']['error'] == 4){
-		$gambar = $gambarlama;
-	} else {
-		$gambar = upload();
-	}
-	
+    //cek apakah use pilih gambar baru atau tidak
+    if ($_FILES['gambar']['error'] == 4) {
+        $gambar = $gambarlama;
+    } else {
+        $gambar = upload();
+    }
 
-	//query insert data
-	$query = "UPDATE hp SET
+    //query insert data
+    $query = "UPDATE hp SET
 			merek ='$merek',
 			tipe = '$tipe',
 			tahun = '$tahun',
 			kondisi = '$kondisi',
 			stok = '$stok',
 			warna = '$warna',
-			gambar = '$gambar' 
+			gambar = '$gambar'
 			WHERE id = $id
 			";
-	mysqli_query($koneksi, $query);
-	return mysqli_affected_rows ($koneksi);
+    mysqli_query($koneksi, $query);
+    return mysqli_affected_rows($koneksi);
 
 }
 
-function cari($keyword) {
-	$query = "SELECT * FROM hp 
+function cari($keyword, $awaldata = null, $Jumlahdataperhalaman = null)
+{
+    $query = "SELECT * FROM hp
 				WHERE
-				merek LIKE '%$keyword%' OR 
-				tipe LIKE '%$keyword%' OR 
-				tahun LIKE '%$keyword%' OR 
-				kondisi LIKE '%$keyword%' OR 
-				stok LIKE '%$keyword%' OR 
-				warna LIKE '%$keyword%' 
+				merek LIKE '%$keyword%' OR
+				tipe LIKE '%$keyword%' OR
+				tahun LIKE '%$keyword%' OR
+				kondisi LIKE '%$keyword%' OR
+				stok LIKE '%$keyword%' OR
+				warna LIKE '%$keyword%'
 
 	";
-	return query($query);
+    //disini abg buat dia cek ada kebutuhan pakai halaman apa engga
+    if (isset($awaldata)) {
+
+        $query = $query . " LIMIT $awaldata, $Jumlahdataperhalaman";
+    }
+    return query($query);
 }
 
-function registerasi ($data) {
-	global $koneksi;
+function registerasi($data)
+{
+    global $koneksi;
 
-	$username = strtolower (stripcslashes($data["username"]));
-	$password = mysqli_real_escape_string($koneksi, $data["password"]);
-	$password2 = mysqli_real_escape_string($koneksi, $data["password2"]);
+    $username = strtolower(stripcslashes($data["username"]));
+    $password = mysqli_real_escape_string($koneksi, $data["password"]);
+    $password2 = mysqli_real_escape_string($koneksi, $data["password2"]);
 
-	//cek username sudah ada atau belum
-	$result = mysqli_query($koneksi, "SELECT username FROM user WHERE username = '$username'");
-	if(mysqli_fetch_assoc($result)) {
-		echo "<script>
+    //cek username sudah ada atau belum
+    $result = mysqli_query($koneksi, "SELECT username FROM user WHERE username = '$username'");
+    if (mysqli_fetch_assoc($result)) {
+        echo "<script>
 				alert('Username Sudah Tersedia')
 			</script>";
-			return false;
-	}
+        return false;
+    }
 
-	
-
-	//cek konfirmasi password
-	if ( $password !== $password2){
-		echo "<script>
+    //cek konfirmasi password
+    if ($password !== $password2) {
+        echo "<script>
 				alert('konfirmasi password Salah!')
 			</script>";
-			return false;
-	}
+        return false;
+    }
 
-	//enkripsi password
-	$password = password_hash($password, PASSWORD_DEFAULT);
+    //enkripsi password
+    $password = password_hash($password, PASSWORD_DEFAULT);
 
-	//tambahkan userbaru ke database
-	mysqli_query($koneksi, "INSERT INTO user VALUES(0, '$username', '$password')");
+    //tambahkan userbaru ke database
+    mysqli_query($koneksi, "INSERT INTO user VALUES(0, '$username', '$password')");
 
-	return mysqli_affected_rows($koneksi);
-
+    return mysqli_affected_rows($koneksi);
 
 }
-
-
-
-
-
-?>
--- ujicoba/index.php
+++ ujicoba/index.php
@@ -1,119 +1,127 @@
-<?php 
+<?php
+
 session_start();
-if ( !isset($_SESSION['login'])) {
-	header("Location: login.php");
-	exit;
+if (!isset($_SESSION['login'])) {
+    header("Location: login.php");
+    exit;
 
 }
 // koneksi ke database
 require "functions.php";
 
-//pagination
+// jika ada keyword
+// ini di ganti get supaya bisa kebaca bersamaan dengan halaman mad
+// terus abg ganti kondisi cek nya cek keyword jadi ga harus berdasarkan tombol cari
+if (isset($_GET["keyword"])) {
+    $data = cari($_GET["keyword"]);
+} else {
+    $data = query("SELECT * FROM hp ");
+}
+
 //konfigurasi
 $Jumlahdataperhalaman = 3;
-$jumlahdata = count(query("SELECT * FROM hp"));
+$jumlahdata = count($data);
 $jumlahhalaman = ceil($jumlahdata / $Jumlahdataperhalaman);
 
 if (isset($_GET["halaman"])) {
-	$halamanaktif = $_GET["halaman"];
-}else {
-	$halamanaktif = 1;
+    $halamanaktif = $_GET["halaman"];
+} else {
+    $halamanaktif = 1;
 }
 
 $awaldata = ($Jumlahdataperhalaman * $halamanaktif) - $Jumlahdataperhalaman;
+//pagination
+// jika ada keyword
 
-
-$data = query ("SELECT * FROM hp LIMIT $awaldata, $Jumlahdataperhalaman");
-
-// jika tombol cari di tekan
-if ( isset($_POST["cari"])) {
-	$data = cari ($_POST["keyword"]);
+if (isset($_GET["keyword"])) {
+    $data = cari($_GET["keyword"], $awaldata, $Jumlahdataperhalaman);
+} else {
+    $data = query("SELECT * FROM hp LIMIT $awaldata, $Jumlahdataperhalaman");
 
 }
-
 ?>
-
-
 <!DOCTYPE html>
 <html>
+
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Halaman Utama</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Halaman Utama</title>
 </head>
+
 <body>
-
-	<a href="logout.php">Logout</a>
-
-	<h1>Daftar Heandphone</h1>
-	<a href="tambah.php">Tambah Data</a>
-	<br> <br>
-
-	<form action="" method="post">
-
-		<input type="text" name="keyword" size="30" autofocus placeholder="Mencari" autocomplete="off">
-		<button type="submit" name="cari">cari</button>
-		
-	</form>
-
-	<br><br>
-
-	<!-- navigasi untuk page -->
-<?php if($halamanaktif > 1) : ?>
-<a href="?halaman=<?= $halamanaktif -1 ;  ?>">&laquo;</a>
-<?php endif; ?>
-
-<?php for( $i = 1; $i <= $jumlahhalaman; $i++) :?>
-		<?php if($i == $halamanaktif) : ?>
-		<a href="?halaman=<?= $i;  ?>" style="font-weight: bold; color:darkblue;"><?= $i;  ?></a>
-	<?php else : ?>
-		<a href="?halaman=<?= $i;  ?>"><?= $i;  ?></a>
-	<?php endif; ?>
-<?php endfor; ?>
-
-<?php if($halamanaktif < $jumlahhalaman) : ?>
-<a href="?halaman=<?= $halamanaktif +1 ;  ?>">&raquo;</a>
-<?php endif; ?>
-
-	<br> <br>
-	<table border="1" cellpadding="10" cellspacing="0">
-		
-		<tr>
-			
-			<th>No.</th>
-			<th>Aksi</th>
-			<th>Gambar</th>
-			<th>Merek</th>
-			<th>Tipe</th>
-			<th>Tahun</th>
-			<th>Kondisi</th>
-			<th>Stok</th>
-			<th>Warna</th>
-
-		</tr>
-
-<?php $i = 1;  ?>
-<?php foreach ($data as $row) : ?>
-		
-		<tr>
-			<td><?= $i;  ?></td>
-			<td>
-				<a href="ubah.php?id=<?= $row["id"];  ?>" >Ubah</a>
-				<a href="hapus.php?id=<?= $row["id"];  ?>" onclick="return confirm ('anda yakin hapus file ini ?');">Hapus</a>
-			</td>
-			<td><img src="img/<?= $row["gambar"];  ?>"></td>
-			<td><?= $row["merek"];  ?></td>
-			<td><?= $row["tipe"];  ?></td>
-			<td><?= $row["tahun"];  ?></td>
-			<td><?= $row["kondisi"];  ?></td>
-			<td><?= $row["stok"];  ?></td>
-			<td><?= $row["warna"];  ?></td>
-			
-		</tr>
-		<?php $i++; ?>
-<?php endforeach; ?>
-
-	</table>
-
+    <a href="logout.php">Logout</a>
+    <h1>Daftar Heandphone</h1>
+    <a href="tambah.php">Tambah Data</a>
+    <br> <br>
+    <!-- btw ini di ganti get supaya bisa muncul di link sama dengan halaman. kalau post dia ga muncul -->
+    <form action="" method="get">
+        <input type="text" name="keyword" value="<?=isset($_GET["keyword"]) ? $_GET["keyword"] : "";?>" size="30" autofocus placeholder="Mencari" autocomplete="off">
+        <button type="submit" name="cari">cari</button>
+    </form>
+    <br><br>
+    <!-- navigasi untuk page -->
+    <?php if ($halamanaktif > 1): ?>
+    <a href="?halaman=<?=$halamanaktif - 1;?><?=isset($_GET["keyword"]) ? "&keyword=" . $_GET["keyword"] : "";?>">&laquo;</a>
+    <?php endif;?>
+    <?php for ($i = 1; $i <= $jumlahhalaman; $i++): ?>
+    <?php if ($i == $halamanaktif): ?>
+    <a href="?halaman=<?=$i;?><?=isset($_GET["keyword"]) ? "&keyword=" . $_GET["keyword"] : "";?>" style="font-weight: bold; color:darkblue;">
+        <?=$i;?></a>
+    <?php else: ?>
+    <a href="?halaman=<?=$i;?><?=isset($_GET["keyword"]) ? "&keyword=" . $_GET["keyword"] : "";?>">
+        <?=$i;?></a>
+    <?php endif;?>
+    <?php endfor;?>
+    <?php if ($halamanaktif < $jumlahhalaman): ?>
+    <a href="?halaman=<?=$halamanaktif + 1;?><?php echo isset($_GET["keyword"]) ? "&keyword=" . $_GET["keyword"] : ""; ?>">&raquo;</a>
+    <?php endif;?>
+    <br> <br>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>Merek</th>
+            <th>Tipe</th>
+            <th>Tahun</th>
+            <th>Kondisi</th>
+            <th>Stok</th>
+            <th>Warna</th>
+        </tr>
+        <?php $i = 1;?>
+        <?php foreach ($data as $row): ?>
+        <tr>
+            <td>
+                <?=$i;?>
+            </td>
+            <td>
+                <a href="ubah.php?id=<?=$row["id"];?>" >Ubah</a>
+                <a href="hapus.php?id=<?=$row["id"];?>" onclick="return confirm ('anda yakin hapus file ini ?');">Hapus</a>
+            </td>
+            <td><img src="img/<?=$row["gambar"];?>"></td>
+            <td>
+                <?=$row["merek"];?>
+            </td>
+            <td>
+                <?=$row["tipe"];?>
+            </td>
+            <td>
+                <?=$row["tahun"];?>
+            </td>
+            <td>
+                <?=$row["kondisi"];?>
+            </td>
+            <td>
+                <?=$row["stok"];?>
+            </td>
+            <td>
+                <?=$row["warna"];?>
+            </td>
+        </tr>
+        <?php $i++;?>
+        <?php endforeach;?>
+    </table>
 </body>
+
 </html>
